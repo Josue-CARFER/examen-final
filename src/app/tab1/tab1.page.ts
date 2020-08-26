7c765dffe772aa78768995f8620b224f52c636c3
JOSUE <carlojosue422@gmail.com>
2020-08-26 20:48:18 +0200

    examen final

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+
+  dado1 = 0;
+  dado2 = 0;
+  dado3 = 0;
+  dado4 = 0;
+  resultado: string;
+
+  constructor() {}
+
+  calcularSuerte() {
+
+    this.dado1 = Math.floor(Math.random() * 3) + 1;
+    this.dado2 = Math.floor(Math.random() * 3) + 1;
+    this.dado3 = Math.floor(Math.random() * 3) + 1;
+    this.dado4 = Math.floor(Math.random() * 3) + 1;
+
+    if(this.dado1  === this.dado2 && this.dado1 === this.dado3 && this.dado1 === this.dado4 )
+    {
+      this.resultado = 'GANASTE ;)';
+    }
+    else
+    {
+      this.resultado = 'PERDISTE :(';
+    }
+
+  }
+
+
+}
